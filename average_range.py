befb7daefdec129b386206dcdf1a1de4e5f25cda
PfaffC <45545667+PfaffC@users.noreply.github.com>
2018-12-02 20:02:59 -0500

    Add files via upload

--- average_range.py
+++ average_range.py
@@ -1,8 +1,7 @@
 import random
 averages = []
 for i in range(0,11):
-    random_n = random.randint(1,101)
-    numbers = list(range(0,random_n))
+    numbers = list(range(0,random.randint(1,101)))
     avg = sum(numbers)/len(numbers)
     averages.append(avg)
 sorted(averages)
